f8d03f5b15b5ae83b1a03751bf8a9fa7d8d2496d
郵差 <twHenryYang@gmail.com>
2021-12-20 19:16:53 +0800

    Connect to MongoDB using dotenv package

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').parse();
+}
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
@@ -9,6 +13,14 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => {
+    console.log(error);
+});
+db.once('open', () => console.log("Connected to Mongo DB~!"));
 
 app.use('/', indexRouter);
 
